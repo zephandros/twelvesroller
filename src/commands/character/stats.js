630627e2f5713e68537d7648aaead683e840aa76
zephandros <twelvesrpg@gmail.com>
2025-04-17 23:57:19 -0600

    updated stats embed

--- src/commands/character/stats.js
+++ src/commands/character/stats.js
@@ -12,10 +12,22 @@ module.exports = {
             .addFields(
                 { name: 'Bloodline', value: 'Your bloodline'},
                 { name: 'Origin', value: 'Your origin' },
+                { name: 'Occupation', value: 'Your current job',  inline: true },
+                { name: 'Points', value: '999',  inline: true },
+                { name: '** **', value: '** **'},
                 { name: 'Physique', value: '12 (+0)',  inline: true },
                 { name: 'Dexterity', value: '12 (+0)',  inline: true },
+                { name: '** **', value: '** **'},
                 { name: 'Intellect', value: '12 (+0)',  inline: true },
                 { name: 'Presence', value: '12 (+0)',  inline: true },
+                { name: '** **', value: '** **'},
+                { name: 'Dodge', value: '12',  inline: true },
+                { name: 'Parry', value: '12',  inline: true },
+                { name: 'Block', value: '12',  inline: true },
+                { name: '** **', value: '** **'},
+                { name: 'Init', value: '12',  inline: true },
+                { name: 'Speed', value: '12',  inline: true },
+                { name: 'Wit', value: '12',  inline: true },
             )
 
         await interaction.reply({embeds: [characterStatEmbed], flags: MessageFlags.Ephemeral });
