c891e5c3806edfa9df415f15714d3bca65b1a757
zephandros <twelvesrpg@gmail.com>
2025-04-17 23:42:19 -0600

    Added embed to stat command.

--- src/commands/character/stats.js
+++ src/commands/character/stats.js
@@ -1,10 +1,23 @@
-const { SlashCommandBuilder, MessageFlags } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('stats')
         .setDescription('Shows your character stats'),
     async execute(interaction) {
-        await interaction.reply({content: 'WIP: Here goes your stats!', flags: MessageFlags.Ephemeral });
+
+        const characterStatEmbed = new EmbedBuilder()
+            .setTitle('Your Character')
+            .setDescription('Some description here')
+            .addFields(
+                { name: 'Bloodline', value: 'Your bloodline'},
+                { name: 'Origin', value: 'Your origin' },
+                { name: 'Physique', value: '12 (+0)',  inline: true },
+                { name: 'Dexterity', value: '12 (+0)',  inline: true },
+                { name: 'Intellect', value: '12 (+0)',  inline: true },
+                { name: 'Presence', value: '12 (+0)',  inline: true },
+            )
+
+        await interaction.reply({embeds: [characterStatEmbed], flags: MessageFlags.Ephemeral });
     }
 };
