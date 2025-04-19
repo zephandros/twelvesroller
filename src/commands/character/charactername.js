cfa2a2c86ebdecd957a33e640d3b3316cec82adf
zephandros <twelvesrpg@gmail.com>
2025-04-18 20:21:55 -0600

    Changes outcome of command, now shows a modal

--- src/commands/character/charactername.js
+++ src/commands/character/charactername.js
@@ -1,4 +1,4 @@
-const { ContextMenuCommandBuilder, ApplicationCommandType, MessageFlags } = require('discord.js');
+const { ContextMenuCommandBuilder, ApplicationCommandType, MessageFlags, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } = require('discord.js');
 /* La idea*/
 module.exports = {
     data: new ContextMenuCommandBuilder()
@@ -7,7 +7,23 @@ module.exports = {
     async execute(interaction) {
         if(interaction.user.id == interaction.targetUser.id){
             const { username } = interaction.targetUser;
-            await interaction.reply({content: `Your name is ${username}` , flags: MessageFlags.Ephemeral });
+
+            const modal = new ModalBuilder().setCustomId('nameModal').setTitle('Character Info')
+
+            const nameInput = new TextInputBuilder()
+                .setCustomId('nameInput')
+                .setLabel('Name')
+                .setValue(username)
+                .setStyle(TextInputStyle.Short)
+
+            
+
+            const nameActionRow = new ActionRowBuilder().addComponents(nameInput)
+
+            modal.addComponents(nameActionRow)
+
+            
+            await interaction.showModal(modal);
         }else{
             await interaction.reply({content: `This command only works with  your own user` , flags: MessageFlags.Ephemeral });
         }
