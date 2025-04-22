cd10091fd5338b95bc526a319ac5adf4aab20892
zephandros <twelvesrpg@gmail.com>
2025-04-22 14:45:49 -0600

    Changed value range of colors.

--- src/utils/color.js
+++ src/utils/color.js
@@ -1,16 +1,16 @@
 class Color {
-    static CYAN = 0xFF00FFE0;       //00FFFF
-    static AZURE = 0xFF009EFF;      //0080FF
-    static BLUE = 0xFF001FFF;       //0000FF
-    static PURPLE = 0xFF6100FF;     //8000FF
-    static MAGENTA = 0xFFE000FF;    //FF00FF
-    static ROSE = 0xFFFF009E;       //FF0080
-    static RED = 0xFFFF001F;        //FF0000
-    static ORANGE = 0xFFFF6100;     //FF8000
-    static YELLOW = 0xFFFFE000;     //FFFF00
-    static LIME = 0xFF9EFF00;       //80FF00
-    static GREEN = 0xFF00E000;      //00FF00
-    static EMERALD = 0xFF00FF80;    //00FF80
+    static CYAN = 0x00FFE0;       //00FFFF
+    static AZURE = 0x009EFF;      //0080FF
+    static BLUE = 0x001FFF;       //0000FF
+    static PURPLE = 0x6100FF;     //8000FF
+    static MAGENTA = 0xE000FF;    //FF00FF
+    static ROSE = 0xFF009E;       //FF0080
+    static RED = 0xFF001F;        //FF0000
+    static ORANGE = 0xFF6100;     //FF8000
+    static YELLOW = 0xFFE000;     //FFFF00
+    static LIME = 0x9EFF00;       //80FF00
+    static GREEN = 0x00E000;      //00FF00
+    static EMERALD = 0x00FF80;    //00FF80
 }
 
 module.exports = { Color }
