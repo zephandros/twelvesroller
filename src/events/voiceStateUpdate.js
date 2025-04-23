d82d09d97182a90032fdce5e9aafb07ae14c1f74
zephandros <twelvesrpg@gmail.com>
2025-04-22 22:18:41 -0600

    Added console logs

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,4 +1,5 @@
-const { Events } = require('discord.js');
+const { Events, channelMention } = require('discord.js');
+const { TWSLogger } = require('../utils/twslogger.js')
 
 module.exports = {
     name: Events.VoiceStateUpdate,
@@ -7,9 +8,13 @@ module.exports = {
         const gameRoomChannelId = "1364451129957814272";
         const playingNowRoleId = "1364450283169517692";
 
+        TWSLogger.log(`Member ${newState.member.displayName} joined channel ${newState.channelId}`);
+
         if(newState && newState.channelId == gameRoomChannelId){
+            TWSLogger.log(`Member ${newState.member.displayName} joined the game room. Applying role...`);
             newState.member.roles.add(playingNowRoleId);
         }else{
+            TWSLogger.log(`Member ${newState.member.displayName} joined another channel. Removing role...`);
             newState.member.roles.remove(playingNowRoleId);
         }
     },
