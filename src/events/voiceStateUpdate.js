95f26098a5e1f7223498e92cd8507addceaf2058
zephandros <twelvesrpg@gmail.com>
2025-04-22 22:29:48 -0600

    Testing

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -7,14 +7,13 @@ module.exports = {
         const gameRoomChannelId = "1364451129957814272";
         const playingNowRoleId = "1364450283169517692";
 
-        TWSLogger.log(`Member ${newState.member.displayName} joined channel ${newState.channelId}`);
-
-        if(newState && newState.channelId == gameRoomChannelId){
-            TWSLogger.log(`Member ${newState.member.displayName} joined the game room. Applying role...`);
-            newState.member.roles.add(playingNowRoleId);
+        if(newState.channelId === null){
+            console.log('user left channel', oldState.channelId);
+        }else if(oldState.channelId === null){
+            console.log('user joined channel', newState.channelId);
         }else{
-            TWSLogger.log(`Member ${newState.member.displayName} joined another channel. Removing role...`);
-            newState.member.roles.remove(playingNowRoleId);
+            console.log('user moved channels', oldState.channelId, newState.channelId)
         }
+
     },
 };
