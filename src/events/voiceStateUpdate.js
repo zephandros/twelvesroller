165301b250c60768a24fcc21bb0ff87e3c58f662
zephandros <twelvesrpg@gmail.com>
2025-04-23 14:06:58 -0600

    Added role validation

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,23 +1,39 @@
-const { Events, channelMention } = require('discord.js');
+const { Events } = require('discord.js');
 const { TWSLogger } = require('../utils/twslogger.js');
 const { Config } = require('../utils/config.js');
 
 module.exports = {
     name: Events.VoiceStateUpdate,
     async execute(oldState, newState) {
-        TWSLogger.log("Triggering event...");
         try{
             if(newState){
+                /* User entered a channel */
+
+                /* Retrieving role */
+                const memberHasRole = newState.member.roles.cache.get(Config.playingNowRoleId) != null;
+
                 if(newState.channelId == Config.gameRoomChannelId){
-                    TWSLogger.log(`Added "Playing now" role to ${newState.member.displayName}.`)
-                    newState.member.roles.add(Config.playingNowRoleId);
+                    /* User entered game room */
+                    if(!memberHasRole){
+                        TWSLogger.log(`Added "Playing now" role to ${newState.member.displayName}.`)
+                        newState.member.roles.add(Config.playingNowRoleId);
+                    }
                 }else{
-                    TWSLogger.log(`Removed "Playing now" role from ${newState.member.displayName}.`)
-                    newState.member.roles.remove(Config.playingNowRoleId);
+                    /* User entered any other channel */
+                    if(memberHasRole){
+                        TWSLogger.log(`Removed "Playing now" role from ${newState.member.displayName}.`)
+                        newState.member.roles.remove(Config.playingNowRoleId);
+                    }
                 }
             } else{
-                TWSLogger.log(`Removed "Playing now" role from ${oldState.member.displayName}.`)
-                oldState.member.roles.remove(Config.playingNowRoleId);
+                /* Member exited a channel*/
+
+                const memberHasRole = oldState.member.roles.cache.get(Config.playingNowRoleId) != null;
+
+                if(memberHasRole){
+                    TWSLogger.log(`Removed "Playing now" role from ${oldState.member.displayName}.`)
+                    oldState.member.roles.remove(Config.playingNowRoleId);
+                }
             }
         }catch(error){
             TWSLogger.logError('Error on member entering or exiting a voice channel', error);
